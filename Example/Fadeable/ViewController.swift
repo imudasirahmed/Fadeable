e2f4903634c22c11189f1a596e9cde66e58040ef
Mudasir Ahmed <mudasir.ahmed@united.com>
2018-01-26 11:38:53 -0600

    Initial commit

---
+++ Example/Fadeable/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Fadeable
+//
+//  Created by imudasirahmed on 01/26/2018.
+//  Copyright (c) 2018 imudasirahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
